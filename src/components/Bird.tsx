10de72805da45452884d6cee256f09c991a9ea1e
Trung Tin <justinvu768@gmail.com~>
2023-05-10 21:10:41 +0700

    fixBird

--- src/components/Bird.tsx
+++ src/components/Bird.tsx
@@ -30,5 +30,5 @@ export default function Bird() {
 const birdStyles: any = {
     position: 'absolute',
     background: `url(${birdImg})`,
-    transition: 'transform 10ms ease-in',
+    transition: 'transform 30ms linear',
 };
