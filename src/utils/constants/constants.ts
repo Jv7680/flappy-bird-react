8b2f87b2e7136b623b57c35bbeddf7b6fdda45e1
Trung Tin <justinvu768@gmail.com~>
2023-06-16 00:54:17 +0700

    change base

--- src/utils/constants/constants.ts
+++ src/utils/constants/constants.ts
@@ -5,4 +5,5 @@ export const REGEX_FULL_NAME = /^[a-z0-9À-ỹ ]+$/i;
 export const REGEX_GMAIL = /^[a-z0-9.]+@gmail\.com$/i;
 
 // API config
-export const BASE_URL = "http://localhost:8080";
+// export const BASE_URL = "http://localhost:8080";
+export const BASE_URL = "https://flappy-bird-react-server.vercel.app";
